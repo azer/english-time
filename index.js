35fe0ad8ed7f0003857be8188ad416452649e2f4
E.Azer Koçulu <azer@kodfabrik.com>
2013-09-23 22:56:12 -0700

    parametrize singular unit names

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var parse   = require("./lib/parse").all,
     units   = require('./lib/units'),
     rewrite = require('./lib/rewrite');
 
-module.exports = function(input, customUnits){
-  return parse(rewrite(input), customUnits || units);
+module.exports = function(input, customUnits, customSingularUnits){
+  return parse(rewrite(input, customSingularUnits), customUnits || units);
 };
 
 module.exports.units = units;
